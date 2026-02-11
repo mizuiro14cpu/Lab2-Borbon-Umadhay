7d25159cc5742efe48d3355fb61677668e88cc4c
mizuiro14cpu <yoseforiel.borbon-22@cpu.edu.ph>
2026-02-11 22:59:08 +0800

    chore: update /utils/db.ts
    - clearDatabase function to include all tables instead of just users
    - commented out original function for backup just to be sure

--- src/__tests__/utils/db.ts
+++ src/__tests__/utils/db.ts
@@ -9,13 +9,36 @@ const supabaseKey = process.env.SUPABASE_KEY!;
 
 export const testClient = createClient(supabaseUrl, supabaseKey);
 
-export const clearDatabase = async () => {
-    const { error } = await testClient
-        .from('users')
-        .delete()
-        .neq('id', 0)
+// export const clearDatabase = async () => {
+//     const { error } = await testClient
+//         .from('users')
+//         .delete()
+//         .neq('id', 0)
     
-        if (error) {
-            throw new Error(`Failed to clear database: ${error.message}`)
-        }
+//         if (error) {
+//             throw new Error(`Failed to clear database: ${error.message}`)
+//         }
+// }
+
+export const clearDatabase = async () => {
+    let error;
+
+    // delete bridge tables first
+    ({ error } = await testClient.from('game_genres').delete().neq('game_id', 0))
+    if (error) {throw new Error(`Failed to clear game_genres: ${error.message}`)}
+
+    ({ error } = await testClient.from('user_games').delete().neq('game_id', 0))
+    if (error) {throw new Error(`Failed to clear user_games: ${error.message}`)}
+
+    ({ error } = await testClient.from('games').delete().neq('id', 0))
+    if (error) {throw new Error(`Failed to clear games: ${error.message}`)}
+
+    ({ error } = await testClient.from('genres').delete().neq('id', 0))
+    if (error) {throw new Error(`Failed to clear genres: ${error.message}`)}
+
+    ({ error } = await testClient.from('users').delete().neq('id', 0))
+    if (error) {throw new Error(`Failed to clear users: ${error.message}`)}
+
+    ({ error } = await testClient.from('developers').delete().neq('id', 0))
+    if (error) {throw new Error(`Failed to clear developers: ${error.message}`)}
 }
